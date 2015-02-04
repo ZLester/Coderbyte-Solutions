631cabe76e065d787b6ae6ab221aa1eaaf1ec248
Zachary Lester <zachary.greg.lester@gmail.com>
2015-02-04 11:45:27 -0800

    Update Easy - 07 - Simple Symbols.js

--- Easy - 07 - Simple Symbols.js
+++ Easy - 07 - Simple Symbols.js
@@ -1,7 +1,30 @@
+// Each step explained
 function SimpleSymbols(str) {
-
+    
+    // First, we declare a variable to hold a regex expression to match all upper and lowercase letters
     var alphaTest = /[a-zA-Z]/;
-
+    
+    // Next, we loop through each character in the string...
+    for (i = 0; i < str.length; i++) {
+        // ... and use the our previously declared regex expression and the .test method to see if the character is a letter
+        if (alphaTest.test(str[i])) {
+            // If our test returns true, we check if either of the adjacent characters are not +'s
+            if ((str[i + 1] != "+") || (str[i - 1] != "+")) {
+                // If one of them isn't, we return false.
+                return false;
+            }
+        }
+    }
+    
+    // If our program is able to check all of the characters and hasn't returned false, we know the string obeys all of the problem's rules and can return true.
+    return true;
+}
+   
+// All together
+function SimpleSymbols(str) {
+    
+    var alphaTest = /[a-zA-Z]/;
+    
     for (i = 0; i < str.length; i++) {
         if (alphaTest.test(str[i])) {
             if ((str[i + 1] != "+") || (str[i - 1] != "+")) {
@@ -12,4 +35,3 @@ function SimpleSymbols(str) {
     
     return true;
 }
-   
