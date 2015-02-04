c2edb6228acb0cf65546f3bf61a2edc4140b6ce9
Zachary Lester <zachary.greg.lester@gmail.com>
2015-02-04 11:10:44 -0800

    Update Easy - 05 - Simple Adding.js

--- Easy - 05 - Simple Adding.js
+++ Easy - 05 - Simple Adding.js
@@ -1,26 +1,27 @@
 // Iterative solution
-function SimpleAdding(num) { 
-
-  var sum = 0;
-  
-  for (var i = 0; i <= num; i++) {
-    sum += i;
-  } 
-  
-  return sum; 
+function SimpleAdding(num) {
+    // Declare a variable to hold our answer
+    var sum = 0;
+    // Loop through all numbers between 0 and num inclusive...
+    for (var i = 0; i <= num; i++) {
+        // ...adding i to our answer each time.
+        sum += i;
+    }
+    // Return our answer.
+    return sum;
 }
 
 // Recursive solution
 function SimpleAdding(num, curSum) {
-  
-  curSum = curSum | 0;
-  
-  if (num === 0){
-    return curSum;
-  }
-  
-  curSum += num;
-  num--;
-  
-  return recursiveAdding(num, curSum);
+
+    curSum = curSum | 0;
+
+    if (num === 0) {
+        return curSum;
+    }
+
+    curSum += num;
+    num--;
+
+    return SimpleAdding(num, curSum);
 }
