8e609e54408a8bb9582ac8d8117fe70c51c639f7
Zachary Lester <zachary.greg.lester@gmail.com>
2015-02-03 16:39:40 -0800

    Update Easy - 02 - First Factorial.js

--- Easy - 02 - First Factorial.js
+++ Easy - 02 - First Factorial.js
@@ -3,21 +3,17 @@
 function FirstFactorial(num) {
    
     // First we declare a for loop starting at the parameter number - 1 (var i = num - 1)
-    // We decrease i by 1 each pass (i--) and stop when i reaches 2 (i > 1)  
-    
+    // We decrease i by 1 each pass (i--) and stop when i reaches 2 (i > 1) 
+    // This loop will have the effect of multiplying num by all whole integers between 2 and num-1 inclusive
+    // For instance, if we pass 4 into our function, the loop will run twice, multiplying 4*3 (12) and then 12*2 (24) 
     for (var i = num - 1; i > 1; i--) {
-        
+       
         // On each pass we multiply the num by i
-        
         num *= i;
         
     }
     
-    // Note that above loop will have the effect of multiplying num by all whole integers between 2 and num-1 inclusive
-    // For instance, if we pass the number 4 into our function, the loop will run twice, multiplying 4*3 (12) and then 12*2 (24) 
-    
     // Make sure to return the number
-
     return num;
 
 }
