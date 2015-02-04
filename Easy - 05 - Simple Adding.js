33a4b0222bf154d14b6f600fddf65c85f3b279f8
Zachary Lester <zachary.greg.lester@gmail.com>
2015-02-03 16:31:39 -0800

    Update Easy - 05 - Simple Adding.js

--- Easy - 05 - Simple Adding.js
+++ Easy - 05 - Simple Adding.js
@@ -1,16 +1,5 @@
-// Changed filetype to .js so syntax highlighting works.
+function SimpleAdding(num) { 
 
-// I see function names suggested on CoderByte as
-// starting with a capital letter, but the 
-// Google style-guide for javascript suggests
-// functionNameLikeThis
-// https://google-styleguide.googlecode.com/svn/trunk/javascriptguide.xml?showone=Naming#Naming
-
-function simpleAdding(num) { 
-
-  // The styleguide also suggests always using 'var'
-  // with declarations.
-  // https://google-styleguide.googlecode.com/svn/trunk/javascriptguide.xml?showone=var#var
   var sum = 0;
   
   if (num < 0){
@@ -25,14 +14,7 @@ function simpleAdding(num) {
 
 }
 
-// We're going to put the fun in functions
-// by solving this recursively!
-
-// Just messing around with github forks and
-// pull requests. Trying to learn and familiarize
-// myself with the process.
-
-function recursiveAdding(num, curSum) {
+function SimpleAdding(num, curSum) {
   curSum = curSum | 0;
   if (num == 0){
     return curSum;
