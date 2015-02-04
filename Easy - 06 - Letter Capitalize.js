8d9d86ae1c438ef8963e875e5c48eb601b745db5
Zachary Lester <zachary.greg.lester@gmail.com>
2015-02-04 11:01:11 -0800

    Update Easy - 06 - Letter Capitalize.js

--- Easy - 06 - Letter Capitalize.js
+++ Easy - 06 - Letter Capitalize.js
@@ -1,16 +1,24 @@
+// With step-by-step instructions
+
 function LetterCapitalize(str) {
-
+    // First, we use the split method to divide the input string into an array of individual words
+    // Note that we pass a string consisting of a single space into the method to "split" the string at each space
     str = str.split(" ");
-
+    
+    // Next, we loop through each item in our new array...
     for (i = 0; i < str.length; i++) {
+        // ...and set each word in the array to be equal to the first letter of the word (str[i][0]) capitalized using the toUpperCase method.
+        // along with a substring of the remainder of the word (passing only 1 arguement into the substr method means that you start at that index and go until the end of the string)
         str[i] = str[i][0].toUpperCase() + str[i].substr(1);
     }
-
+    // Finally, we join our array back together...
     str = str.join(" ");
-
+    
+    // ...and return our answer.
     return str;
 }
 
+// With best practices
 function LetterCapitalize(str) {
 
     str = str.split(" ");
