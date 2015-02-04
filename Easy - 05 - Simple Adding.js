259a553ece83083eab5b52d5a721268b21852c27
Zachary Lester <zachary.greg.lester@gmail.com>
2015-02-04 11:03:11 -0800

    Update Easy - 05 - Simple Adding.js

--- Easy - 05 - Simple Adding.js
+++ Easy - 05 - Simple Adding.js
@@ -1,25 +1,28 @@
+// Iterative solution
+
 function SimpleAdding(num) { 
 
   var sum = 0;
   
-  if (num < 0){
-    throw "Number must be non-negative.";
-  }
-
   for (var i = 0; i <= num; i++) {
     sum += i;
   } 
   
   return sum; 
-
 }
 
+// Recursive solution
+
 function SimpleAdding(num, curSum) {
+  
   curSum = curSum | 0;
+  
   if (num == 0){
     return curSum;
   }
+  
   curSum += num;
   num--;
+  
   return recursiveAdding(num, curSum);
 }
