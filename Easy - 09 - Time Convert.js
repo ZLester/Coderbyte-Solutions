8b90005728d2e5c8ae87f9ea573e3260e432551e
Zachary Lester <zachary.greg.lester@gmail.com>
2015-02-05 13:20:13 -0800

    Update Easy - 09 - Time Convert.js

--- Easy - 09 - Time Convert.js
+++ Easy - 09 - Time Convert.js
@@ -1,9 +1,10 @@
 // With step by step instructions
-
 function TimeConvert(num) {
+    
     // First, we divide our number by 60 and then use math.floor to "round down" and get the number of hours.
     // Note that Math.floor returns the nearest whole integer less than or equal to the input number.
     var hours = Math.floor(num / 60);
+    
     // Next, we use the modulus operator to figure out the number of minutes (the remainder of num/60). 
     var minutes = num % 60;
     
@@ -12,7 +13,6 @@ function TimeConvert(num) {
 }
 
 // Best practices
-
 function TimeConvert(num) {
 
     return Math.floor(num / 60) + ":" + num % 60;
