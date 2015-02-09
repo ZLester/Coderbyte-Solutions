e728f12b2f3856e92828ea28623d5ab632c5eae6
Zachary Lester <zachary.greg.lester@gmail.com>
2015-02-09 00:20:53 -0800

    Update and rename Easy - 10 - Alphabet Soup to Easy - 10 - Alphabet Soup.js

---
+++ Easy - 10 - Alphabet Soup.js
@@ -0,0 +1,23 @@
+// Step By Step
+function AlphabetSoup(str) { 
+  
+  // Use .split to turn the string into an array 
+  str = str.split("");
+  
+  // .sort to alphabatize the new array...
+  str = str.sort();
+  
+  // and .join to turn it back into a string.
+  str = str.join("");
+  
+  // Then, return your answer.
+  return str;
+
+}
+
+// Best Practices
+function AlphabetSoup(str) { 
+  
+  return str.split("").sort().join("");
+
+}
