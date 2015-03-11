03323199e0d08e5bad225c32289c9769c21a9fff
Zachary Lester <zachary.greg.lester@gmail.com>
2015-03-11 01:56:11 -0700

    Update Easy - 25 - Number Addition.js

--- Easy - 25 - Number Addition.js
+++ Easy - 25 - Number Addition.js
@@ -1,15 +1,21 @@
 // Step By Step
 function NumberAddition(str) {
-
+    // First we charge our input string into an array by splitting it at every alphabetic character.
+    // Note that this regex expression will match any capital or lowercase letter.
     str = str.split(/[a-zA-Z]/);
+    // Next, we declare a sum variable to hold our answer.
     var sum = 0;
-
+    
+    // After that, we loop through each element in our modified array...
     for (var i = 0; i < str.length; i++) {
+        // ...and if it isn't an empty string...
         if (str[i] != "") {
+            // ...we parse its value with parseInt and add it to our answer.
             sum += parseInt(str[i]);
         }
     }
-
+    
+    // Finally, we return our answer
     return sum;
 }
 
