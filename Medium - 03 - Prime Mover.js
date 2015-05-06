19f5423d976ae6301f64affb10b6ece37a550e8a
Zachary Lester <zachary.greg.lester@gmail.com>
2015-05-06 13:36:47 -0700

    Correct error in new solution

--- Medium - 03 - Prime Mover.js
+++ Medium - 03 - Prime Mover.js
@@ -4,7 +4,7 @@ function PrimeMover(mynum) {
         var isPrime = true;
         for (var i = 2; i < num; i++) {
             if (num % i === 0) {
-                var isPrime = false;
+                isPrime = false;
             }
         }
         return isPrime;
@@ -14,7 +14,7 @@ var primeArr = [2];
 
 for (var k = 2; k < 10000; k++) {
     if (test(k)) {
-        primeArr.push(test(k));
+        primeArr.push(k);
     }
 }
 
