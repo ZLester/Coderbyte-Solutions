f532bffcb18f8fe84415ba91f4dd5d9fe9bdbb01
Zachary Lester <zachary.greg.lester@gmail.com>
2015-07-06 11:50:40 -0700

    Update Medium - 01 - Prime Time.js

--- Medium - 01 - Prime Time.js
+++ Medium - 01 - Prime Time.js
@@ -1,14 +1,11 @@
 function PrimeTime(num) {
-    if (num == 1) {
+    if (num === 1) {
         return false;
     }
-
     for (var i = 2; i < num; i++) {
         if (num % i === 0) {
             return false;
         }
     }
-
     return true;
-
 }
