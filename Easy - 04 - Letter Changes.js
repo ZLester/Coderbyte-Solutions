cbc8e1298a20ee7db367f43596f9c48575451b60
Zachary Lester <zachary.greg.lester@gmail.com>
2015-08-19 18:02:40 -0700

    Update Easy - 04 - Letter Changes.js

--- Easy - 04 - Letter Changes.js
+++ Easy - 04 - Letter Changes.js
@@ -7,36 +7,29 @@ function LetterChanges(str) {
     // we can "cheat" a by making alpha[3] equal to "d" and newAlpha[3] equal to "E".
     var alpha = "abcdefghijklmnopqrstuvwxyz";
     var newAlpha = "bcdEfghIjklmnOpqrstUvwxyzA";
-    
     // Next, we declare a variable to hold our answer
     var answer = "";
     
     // After that, we loop through each character in our input string
     for (i = 0; i < str.length; i++) {
-        
         // First, we use the indexOf method to check if the current character in our string is contained in alpha.
         // Note that if the string you pass into indexOf isn't found, it will return -1. Otherwise, it will return the index of the first matching character found.
         // For instance, alpha.indexOf("c") returns 2, while alpha.indexOf("C") returns -1. 
         if (alpha.indexOf(str[i]) !== -1) {
-            
             // If we find the character in the alpha string, we declare a variable to hold the index of the character.
             // Note that this is an unnessary step that I do for the purposes of clarity. See the 2nd function for a faster implementation.
             var index = alpha.indexOf(str[i]);
-            
             // Since we set up the characters in alpha to have the same index as the one we want to convert it to in newAlpha,
             // all we have to do is use the charAt method to add the converted character to our answer variable.
             answer += newAlpha.charAt(index);
-        
         // If str[i] doesn't appear in alpha...
         } else {
-            
             // ...we add it to our answer string, leaving any characters we don't want to change untouched and in the same index in our answer variable as they were in our input string.
             answer += str[i];
         }
     }
-    // Finally, we return the variable where we stored our answer.
+    // Finally, we return our answer.
     return answer;
-
 }
 
 // Best Practices
@@ -53,6 +46,5 @@ function LetterChanges(str) {
             answer += str[i];
         }
     }
-    
     return answer;
 }
