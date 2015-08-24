ccef1ca324a9106378e4f4ff4eeab9d0b87ce7da
Zachary Lester <zachary.greg.lester@gmail.com>
2015-08-24 15:56:00 -0700

    Update Medium - 19 - Swap II.js

--- Medium - 19 - Swap II.js
+++ Medium - 19 - Swap II.js
@@ -3,25 +3,24 @@ function SwapII(str) {
   var testChar = /[a-zA-Z]/;
   var testSwap = /[0-9][a-zA-Z]+[0-9]/g;
   var answer = "";
-  for (i=0;i<str.length;i++) {
+  for (var i=0; i < str.length; i++) {
     if (testChar.test(str[i])) {
       if (str[i] == str[i].toUpperCase()) {
-      answer += str[i].toLowerCase();
+        answer += str[i].toLowerCase();
       }
       else if (str[i] == str[i].toLowerCase()) {
-      answer += str[i].toUpperCase();
+        answer += str[i].toUpperCase();
       }
       }
     else {
-    answer += str[i];
+      answer += str[i];
     }
   }
   var numSwaps = answer.match(testSwap);
   if (numSwaps !== null) {
-    for (i=0;i<numSwaps.length;i++) {
-    answer = answer.replace(numSwaps[i],numSwaps[i][numSwaps[i].length-1]+numSwaps[i].substr(1,numSwaps[i].length-2)+numSwaps[i][0]);
+    for (var j = 0; j < numSwaps.length; j++) {
+      answer = answer.replace(numSwaps[j], numSwaps[j][numSwaps[j].length-1]+numSwaps[j].substr(1,numSwaps[j].length-2)+numSwaps[j][0]);
     }
   }
   return answer; 
-         
 }
