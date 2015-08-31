6262f347c8886c8b6dedaad771140b12ad8f4521
Zachary Lester <zachary.greg.lester@gmail.com>
2015-08-30 18:24:46 -0700

    Update Medium - 02 - Run Length.js

--- Medium - 02 - Run Length.js
+++ Medium - 02 - Run Length.js
@@ -2,19 +2,15 @@ function RunLength(str) {
 
     str = str.split("");
     var count = 1;
-
     var answer = "";
 
-    for (var i = 0, length = str.length; i < length; i++) {
+    for (var i = 0; i < tsr.length; i++) {
         if (str[i] === str[i + 1]) {
             count++;
         } else {
             answer += count.toString() + str[i].toString();
             count = 1;
         }
-
     }
-
     return answer;
-
 }
