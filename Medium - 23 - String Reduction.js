9bc439defaf5daa461e154dee94fdddea5f1bd83
Zachary Lester <zachary.greg.lester@gmail.com>
2015-08-30 18:23:39 -0700

    Update Medium - 23 - String Reduction.js

--- Medium - 23 - String Reduction.js
+++ Medium - 23 - String Reduction.js
@@ -2,9 +2,9 @@ function StringReduction(str) {
   
   str = str.split("");
   
-  function notAllSame (arr) {
-    for (i=0;i<str.length;i++) {
-      if (str[0] == str[i]) {
+  function notAllSame() {
+    for (var j = 0; j < str.length; j++) {
+      if (str[0] === str[j]) {
         continue;
       }
       else {
@@ -13,31 +13,28 @@ function StringReduction(str) {
     }
     return false;
   }
-  while (notAllSame(str)) {
   
-
-  for (i=0;i<str.length;i++) {
-    if (str[i] + str[i+1] == "ab") {
-    str.splice(i,2,"c");
+  while (notAllSame()) {
+    for (var i = 0; i < str.length; i++) {
+      if (str[i] + str[i+1] == "ab") {
+        str.splice(i,2,"c");
+      }
+      if ((str[i] === "a") && (str[i+1] == "c")) {
+        str.splice(0,2,"b");
+      }
+      if ((str[i] === "b") && (str[i+1] == "c")) {
+        str.splice(i,2,"a");
+      }
+      if ((str[i] === "b") && (str[i+1] == "a")) {
+        str.splice(i,2,"c");
+      }
+      if ((str[i] === "c") && (str[i+1] == "b")) {
+        str.splice(i,2,"a");
+      }
+      if ((str[i] === "c") && (str[i+1] == "a")) {
+        str.splice(i,2,"b");
+      }
     }
-    if ((str[i] == "a") && (str[i+1] == "c")) {
-    str.splice(0,2,"b");
-    }
-    if ((str[i] == "b") && (str[i+1] == "c")) {
-    str.splice(i,2,"a");
-    }
-    if ((str[i] == "b") && (str[i+1] == "a")) {
-    str.splice(i,2,"c");
-    }
-    if ((str[i] == "c") && (str[i+1] == "b")) {
-    str.splice(i,2,"a");
-    }
-    if ((str[i] == "c") && (str[i+1] == "a")) {
-    str.splice(i,2,"b");
-    }
-  }
-  
   }
   return str.length;
-         
 }
