96c886d146b9585a23f9c91819ee4ffa0e55dda9
Zachary Lester <zachary.greg.lester@gmail.com>
2015-10-06 13:38:14 -0700

    Update Medium - 19 - Swap II.js

--- Medium - 19 - Swap II.js
+++ Medium - 19 - Swap II.js
@@ -1,25 +1,36 @@
-function SwapII(str) { 
+function SwapII(str) {
+  // Regex to match any upper or lowercase character
   var testChar = /[a-zA-Z]/;
+  // Regex to match a number, followed by any number (but at least one) alpha characters, followed by a number
   var testSwap = /[0-9][a-zA-Z]+[0-9]/g;
-  var answer = "";
-  for (var i=0; i < str.length; i++) {
+  var result = "";
+  
+  // Loop over the input string...
+  for (var i = 0; i < str.length; i++) {
+    // If the character is alphabetic...
     if (testChar.test(str[i])) {
+      // ...and change it to lowercase if it's uppercase, and uppercase if it's lowercase
       if (str[i] == str[i].toUpperCase()) {
-        answer += str[i].toLowerCase();
+        result += str[i].toLowerCase();
       }
       else if (str[i] == str[i].toLowerCase()) {
-        answer += str[i].toUpperCase();
+        result += str[i].toUpperCase();
       }
       }
+    // If it's some other character, add it to our result string.
     else {
-      answer += str[i];
+      result += str[i];
     }
   }
-  var numSwaps = answer.match(testSwap);
+  // Next, we use the .match method to return all substrings that meet the criteria of testSwap.
+  var numSwaps = result.match(testSwap);
+  // If there are matches...
   if (numSwaps !== null) {
+    // Loop over the array of substrings and replace each one with the same string's first and last characters swapped
     for (var j = 0; j < numSwaps.length; j++) {
-      answer = answer.replace(numSwaps[j], numSwaps[j][numSwaps[j].length-1]+numSwaps[j].substr(1,numSwaps[j].length-2)+numSwaps[j][0]);
+      result = result.replace(numSwaps[j], numSwaps[j][numSwaps[j].length-1]+numSwaps[j].substr(1,numSwaps[j].length-2)+numSwaps[j][0]);
     }
   }
-  return answer; 
+  // Finally, we return our result
+  return result; 
 }
