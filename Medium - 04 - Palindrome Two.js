3daccf319d61d55756aa556ad12d5a093f145366
Zachary Lester <zachary.greg.lester@gmail.com>
2015-10-13 10:55:29 -0700

    Update Medium - 04 - Palindrome Two.js

--- Medium - 04 - Palindrome Two.js
+++ Medium - 04 - Palindrome Two.js
@@ -1,6 +1,9 @@
 function PalindromeTwo(str) { 
+  var strFor = str.replace(/[^a-zA-Z]/g, "").toLowerCase();
+  var strRev =  str.replace(/[^a-zA-Z]/g, "").split("").reverse().join("").toLowerCase();
+  return strFor === strRev;
+}
 
-  
-  return str.replace(/[^a-zA-Z]/g, "").toLowerCase() === str.replace(/[^a-zA-Z]/g, "").split("").reverse().join("").toLowerCase(); 
-         
+function PalindromeTwo(str) { 
+  return str.replace(/[^a-zA-Z]/g, "").toLowerCase() === str.replace(/[^a-zA-Z]/g, "").split("").reverse().join("").toLowerCase();
 }
